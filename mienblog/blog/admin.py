19857ce3e3a3b16cd0283bcc1bf3faeacde46869
airborne101st <akshatsharmatheking@gmail.com>
2024-02-17 22:21:48 +0530

    [ADD] Registered Tree view for Post model in admin

--- mienblog/blog/admin.py
+++ mienblog/blog/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Author, Category
 
 # Register your models here.
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_at')
+    list_filter = ("status",)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author)
+admin.site.register(Category)
