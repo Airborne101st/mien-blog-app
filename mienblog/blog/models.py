061b9680fb394fe61c2ad70f39bb229ac1786a45
airborne101st <akshatsharmatheking@gmail.com>
2024-02-17 22:23:35 +0530

    [ADD] Added a status field in Post model

--- mienblog/blog/models.py
+++ mienblog/blog/models.py
@@ -1,16 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+STATUS = (
+    (0, "Draft"),
+    (1, "Publish")
+)
+
+
 class Category(models.Model):
     name = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
 
 class Author(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -20,7 +30,8 @@ class Post(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='blog_posts')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    published_at = models.DateTimeField(blank=True)
+    published_at = models.DateTimeField(blank=True, null=True)
+    status = models.IntegerField(choices=STATUS, default=0)
 
     class Meta:
         ordering = ['-created_at']
